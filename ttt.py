c00c1351ef03b5b4209911e5573ea2c9a0487a62
ltaustinrobb <ltaustinrobb@gmail.com>
2017-09-27 15:07:24 +0000

    knots and crosses half done

---
+++ ttt.py
@@ -0,0 +1,35 @@
+#Tic Tac Toe
+
+board =[0,1,2,
+        3,4,5,
+        6,7,8]
+
+def show():
+  print board[0],'|',board[1],'|',board[2]
+  print "----------"
+  print board[3],'|',board[4],'|',board[5]
+  print "----------"
+  print board[6],'|',board[7],'|',board[8]
+
+show()
+while True:
+  input = raw_input("Player 1 Select a spot: ")
+  input = int(input)
+
+  if board[input] != 'x' and board[input] != 'o':
+    board[input] = 'x'
+  else:
+    print "This spot is taken"
+  show()
+
+  input2 = raw_input("Player 2 Select a spot: ")
+  input2 = int(input2)
+
+  if board[input2] != 'x' and board[input2] != 'o':
+    board[input2] = 'o'
+  else:
+    print "This spot is taken"
+  show()
+
+
+
