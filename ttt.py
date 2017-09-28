b89eb19fe7e905c93f722299c64293824947f80a
ltaustinrobb <ltaustinrobb@gmail.com>
2017-09-28 13:34:54 +0000

    functioning o&xs

--- ttt.py
+++ ttt.py
@@ -3,6 +3,11 @@
 board =[0,1,2,
         3,4,5,
         6,7,8]
+xs= []
+os = []
+wins = [[0,1,2],[3,4,5],[6,7,8],
+        [1,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]]
 
 def show():
   print board[0],'|',board[1],'|',board[2]
@@ -18,6 +23,8 @@ def p1turn():
 
   if board[input] != 'x' and board[input] != 'o':
     board[input] = 'x'
+    xs.append(input)
+    xs.sort()
   else:
     print "This spot is taken"
     p1turn()
@@ -29,15 +36,26 @@ def p2turn():
 
   if board[input2] != 'x' and board[input2] != 'o':
     board[input2] = 'o'
+    os.append(input)
+    os.sort()
   else:
     print "This spot is taken"
     p2turn()
 
+def checkwin():
+  if xs in wins:
+    print "Player 1 wins"
+    quit()
+  if os in wins:
+    print "Player 2 wins"
+    quit()
 show()
 while True:
   p1turn()
+  checkwin()
   show()
   p2turn()
+  checkwin()
   show()
 
 
